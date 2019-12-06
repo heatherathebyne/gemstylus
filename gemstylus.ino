b15df1e135000776b831861d943b64fea2926bed
Heather Athebyne <heather@athebyne.com>
2019-12-06 18:29:33 -0500

    Change to use 20x4 LCD

--- gemstylus.ino
+++ gemstylus.ino
@@ -19,12 +19,10 @@ float offset = 1.7;
 
 
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include "Adafruit_LEDBackpack.h"
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 #include <MPU6050.h>
 
-// Initialize the 7-segment display and accelerometer
-Adafruit_7segment matrix = Adafruit_7segment();
 MPU6050 mpu;
 Vector  normAccel;
 
@@ -33,40 +31,18 @@ int16_t angle = 0;
 int16_t last_angle = 0;
 uint8_t pollcount  = 70;
 
-// 0-9
-const byte num[10] = {
-  B00111111, B00000110, B01011011, B01001111, B01100110, // 01234
-  B01101101, B01111101, B00000111, B01111111, B01101111  // 56789
-};
-
-// A-Z
-const byte letter[27] = {
-  B01110111, B01111100, B01011000, B01011110, B01111001, // ABCDE
-  B01110001, B01101111, B01110100, B00110000, B00001110, // FGHIJ
-  B11110110, B00111000, B11010100, B01010100, B01011100, // KLMNO
-  B01110011, B01100111, B01010000, B01101101, B01111000, // PQRST
-  B00011100, B10011100, B10011100, B01110110, B01101110, // UVWXY
-  B01011011, B00000000                                   // Z
-};
-
 void setup() {
   Wire.begin();
-
-  matrix.begin(0x70);
-  matrix.writeDigitRaw(0, letter[1]);  // b
-  matrix.writeDigitRaw(1, letter[14]); // o
-  matrix.drawColon(false);
-  matrix.writeDigitRaw(3, letter[14]); // o
-  matrix.writeDigitRaw(4, letter[19]); // t
-  matrix.writeDisplay();
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(4, 1);
+  lcd.print("Booting...");
 
   // Stall until the MPU sensor picks up, grab 255 gyro readings,
   // and wait for it to stabilize
   while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G)){}
   mpu.calibrateGyro(255);
   delay(100);
-
-  Serial.begin(115200);
 }
 
 void loop() {
@@ -86,7 +62,7 @@ void loop() {
   // Calculate the pitch to find our angle.
   // We cast it to a double while multiplying by 10 to limit our result
   // to one decimal
-  angle = ((-(atan2(ax, sqrt(ay * ay + az * az)) * 180.0) / M_PI) + 90) * 10;
+  angle = ((-(atan2(ax, sqrt(ay * ay + az * az)) * 180.0) / M_PI) + 90) * 100;
 
   // Calculate the roll to detect when the stylus moves past zero
   int16_t roll = (atan2(ay, az) * 180.0) / M_PI;
@@ -105,7 +81,12 @@ void loop() {
 void updateDigits() {
   // Cast the angle to absolute (removes negative for easy math).
   uint16_t temp = abs(angle);
+
+  lcd.setCursor(0, 0);
+  lcd.print(temp);
+  lcd.print("          ");
   
+  /*
   // Write the last two digits, which require no processing
   matrix.writeDigitRaw(4, num[temp % 10]);
   matrix.writeDigitRaw(3, num[(temp / 10 % 10)] | 0x80); // decimal point
@@ -129,4 +110,5 @@ void updateDigits() {
 
   // Update the display
   matrix.writeDisplay();
+  */
 }
