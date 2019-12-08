fc2ca02053013cc1e6accfacb193c7f52dbda435
Heather Athebyne <heather@athebyne.com>
2019-12-07 19:19:15 -0500

    Bunch of changes for my Lee machine and particular hardware

--- gemstylus.ino
+++ gemstylus.ino
@@ -13,48 +13,57 @@
  *    and/or other materials provided with the distribution. 
  */
 
-// Set this to zero and compile the sketch. Position the stylus at zero degrees
-// relative to the faceting lap and record the value here.
-float offset = 1.7;
-
-
 #include <Wire.h>
+// MPU6050 lib from https://github.com/jrowberg/i2cdevlib/
+#include "I2Cdev.h"
+#include "MPU6050.h"
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 20, 4);
-#include <MPU6050.h>
 
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 MPU6050 mpu;
-Vector  normAccel;
 
 // Used throughout the loops
 int16_t angle = 0;
 int16_t last_angle = 0;
-uint8_t pollcount  = 70;
+unsigned int pollcount  = 500;
+
+int16_t axr, ayr, azr;
 
 void setup() {
   Wire.begin();
+  mpu.initialize();
+  
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(4, 1);
-  lcd.print("Booting...");
+  lcd.setCursor(0,1);
+  lcd.print(mpu.testConnection() ? "Connected to faceter" : "Faceter conn failed");
 
-  // Stall until the MPU sensor picks up, grab 255 gyro readings,
-  // and wait for it to stabilize
-  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G)){}
-  mpu.calibrateGyro(255);
-  delay(100);
+  // Update internal sensor offsets for my specific MPU6050
+  // Yours will vary
+  mpu.setXAccelOffset(-2742);
+  mpu.setYAccelOffset(-2422);
+  mpu.setZAccelOffset(1076);
+  mpu.setXGyroOffset(69);
+  mpu.setYGyroOffset(149);
+  mpu.setZGyroOffset(41);
+
+  // Digital low-pass filter mode
+  mpu.setDLPFMode(6); // 5Hz
+  // Accelerometer sensitivity
+  mpu.setFullScaleAccelRange(0);  
 }
 
 void loop() {
-  double ax = 0, ay = 0, az = 0;
+  long ax = 0, ay = 0, az = 0;
 
   // Collect pollcount sensor readings and average each axis
-  for(int8_t i = 0; i < pollcount; i++) {
-    normAccel = mpu.readNormalizeAccel();
-    ax+=normAccel.XAxis;
-    ay+=normAccel.YAxis;
-    az+=normAccel.ZAxis;
+  for(uint16_t i = 0; i < pollcount; i++) {
+    mpu.getAcceleration(&axr, &ayr, &azr);
+    ax+=axr;
+    ay+=ayr;
+    az+=azr;
   }
+
   ax = ax / pollcount;
   ay = ay / pollcount;
   az = az / pollcount;
@@ -62,53 +71,20 @@ void loop() {
   // Calculate the pitch to find our angle.
   // We cast it to a double while multiplying by 10 to limit our result
   // to one decimal
-  angle = ((-(atan2(ax, sqrt(ay * ay + az * az)) * 180.0) / M_PI) + 90) * 100;
+  angle = ((-(atan2(ay, sqrt(ax * ax + az * az)) * 180.0) / M_PI) + 90.0) * 100.0;
 
   // Calculate the roll to detect when the stylus moves past zero
   int16_t roll = (atan2(ay, az) * 180.0) / M_PI;
   if(roll < 0) {
     angle = angle * -1;
+  
   }
 
-  // Add our known offset from zero
-  angle += (offset * 10);
-
-  updateDigits();
-}
-
-
-// Split up the digits and draw them to the LED display
-void updateDigits() {
   // Cast the angle to absolute (removes negative for easy math).
-  uint16_t temp = abs(angle);
+  double absangle = abs(angle) / 100.0;
 
+  // Write to the display
   lcd.setCursor(0, 0);
-  lcd.print(temp);
+  lcd.print(String(absangle,2));
   lcd.print("          ");
-  
-  /*
-  // Write the last two digits, which require no processing
-  matrix.writeDigitRaw(4, num[temp % 10]);
-  matrix.writeDigitRaw(3, num[(temp / 10 % 10)] | 0x80); // decimal point
-
-  // Dirty code to blank the upper digits if insignificant,
-  // write the number, or write a negative symbol.
-  if((temp / 100 % 10) > 0)
-    matrix.writeDigitRaw(1, num[(temp / 100 % 10)]);
-  else if((temp / 100 % 10) == 0 && temp > 100)
-    matrix.writeDigitRaw(1, num[0]);
-  else if(angle < 0)
-    matrix.writeDigitRaw(1, B01000000);
-  else
-    matrix.writeDigitRaw(1, 0);
-  if((temp / 1000 % 10) > 0)
-    matrix.writeDigitRaw(0, num[(angle / 1000 % 10)]);
-  else if(angle < 0 && (temp / 100 % 10) > 0)
-    matrix.writeDigitRaw(0, B01000000);
-  else
-    matrix.writeDigitRaw(0, 0);
-
-  // Update the display
-  matrix.writeDisplay();
-  */
 }
